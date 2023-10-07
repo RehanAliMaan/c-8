d94aa94047a28793cc3ac4dddd87aff0a3c0de51
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-10-07 21:47:26 +0500

    Add files via upload

---
+++ Untitled9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	cout<<"input:"<<endl;
+	cout<<"10"<<" "<<"31"<<" "<<"2"<<" "<<"73"<<" "<<"24"<<" "<<" 65"<<" "<<"6"<<" "<<"87"<<" "<<"18"<<endl;
+	cout<<"output"<<endl;
+	cout<<"10"<<" "<<"31"<<" "<<"2"<<endl;
+	cout<<"73"<<" "<<"24"<<" "<<"65"<<endl;
+	cout<<"6"<<"  "<<"87"<<"  "<<"18"<<endl;
+}
